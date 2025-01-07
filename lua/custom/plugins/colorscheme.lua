57d8599dc041eb535c7b4f33081585bf36d5eb75
Andrew Marek <andwmarek@gmail.com>
2025-01-06 17:12:41 -0800

    make the config more readable and sync lazyvim

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,20 +1,31 @@
-return { -- You can easily change to a different colorscheme.
-  -- Change the name of the colorscheme plugin below, and then
-  -- change the command in the config to whatever the name of that colorscheme is.
-  --
-  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  'EdenEast/nightfox.nvim',
-  'folke/tokyonight.nvim',
-  'rebelot/kanagawa.nvim',
-  'ellisonleao/gruvbox.nvim',
-  priority = 1000, -- Make sure to load this before all the other start plugins.
-  init = function()
-    -- Load the colorscheme here.
-    -- Like many other themes, this one has different styles, and you could load
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'tokyonight-night'
-
-    -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
-  end,
+return {
+  -- Colorscheme plugins
+  {
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
+  },
+  {
+    'folke/tokyonight.nvim',
+    priority = 1000,
+  },
+  {
+    'EdenEast/nightfox.nvim',
+    priority = 1000,
+  },
+  {
+    'catppuccin/nvim',
+    config = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles
+      vim.cmd.colorscheme 'catppuccin-mocha'
+      -- Configure highlights
+      vim.cmd.hi 'Comment gui=none'
+    end,
+    name = 'catppuccin',
+    priority = 1000,
+  },
+  {
+    'rebelot/kanagawa.nvim',
+    priority = 1000,
+  },
 }
